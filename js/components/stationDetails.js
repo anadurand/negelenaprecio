afee1a814c144d92d8a6ad40947805afcc71981b
anadurand <ana.dc1004@gmail.com>
2019-12-03 23:00:25 -0500

    precios

--- js/components/stationDetails.js
+++ js/components/stationDetails.js
@@ -3,13 +3,13 @@ const stationDetail = (update) => {
   const parent = $('<div class="station-detail"></div>');
   const h2 = $('<h2 class="name-station">'+ state.selectedStation.producto + '</h2>');
   const hr = $('<hr>');
-  const pAddress = $('<p class="direccion">Unidad:' + state.selectedStation.Unidad + '</p>');
-  const pDecena = $('<p class="direccion">Denena:' + state.selectedStation.Decena + '</p>');
-  const pDocena = $('<p class="direccion">Docena:' + state.selectedStation.Docena + '</p>');
-  const pCiento = $('<p class="direccion">Ciento:' + state.selectedStation.Ciento + '</p>');
-  //const pGruesa = $('<p class="direccion">Gruesa:' + state.selectedStation.Gruesa + '</p>');
-  //const pMillar = $('<p class="direccion">Millar:' + state.selectedStation.Millar + '</p>');
-  const pCaja = $('<p class="direccion">Caja:' + state.selectedStation.Caja + '</p>');
+  const pAddress = $('<p class="direccion">Unidad:' + state.selectedStation.unidad + '</p>');
+  const pDecena = $('<p class="direccion">Denena:' + state.selectedStation.decena + '</p>');
+  const pDocena = $('<p class="direccion">Docena:' + state.selectedStation.docena + '</p>');
+  const pCiento = $('<p class="direccion">Ciento:' + state.selectedStation.ciento + '</p>');
+  const pGruesa = $('<p class="direccion">Gruesa:' + state.selectedStation.gruesa + '</p>');
+  const pMillar = $('<p class="direccion">Millar:' + state.selectedStation.millar + '</p>');
+  const pCaja = $('<p class="direccion">Caja:' + state.selectedStation.caja + '</p>');
 
 
   
@@ -19,8 +19,8 @@ const stationDetail = (update) => {
   parent.append(pDecena);
   parent.append(pDocena);
   parent.append(pCiento);
-  //parent.append(pGruesa);
-  //parent.append(pMillar);
+  parent.append(pGruesa);
+  parent.append(pMillar);
   parent.append(pCaja);
   return parent;
 }
